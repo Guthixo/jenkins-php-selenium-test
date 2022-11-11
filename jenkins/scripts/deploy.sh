47beb0d296fdbf786209a0edd0a837c37f669df2
Lee Wei Jie <weijie899@hotmail.com>
2022-11-11 22:30:39 +0800

    add initial files

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,6 +5,7 @@ docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium
 sleep 1
 set +x
 
+
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
 
